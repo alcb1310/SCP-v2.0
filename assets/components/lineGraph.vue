c06ddbe43033f4c2aa84497d1cc01fd40641a6c0
alcb1310 <97707578+alcb1310@users.noreply.github.com>
2022-03-06 11:30:31 -0500

    removing vue-chartjs

--- assets/components/lineGraph.vue
+++ assets/components/lineGraph.vue
@@ -1,17 +1,5 @@
 <script>
-import { Line } from 'vue-chartjs';
-
 export default {
   name: 'LineGraph',
-  extends: Line,
-  data() {
-    return {
-      myChart: null,
-    };
-  },
-  props: ['chartdata', 'options'],
-  mounted() {
-    this.renderChart(this.chartdata, this.options);
-  },
 };
 </script>
