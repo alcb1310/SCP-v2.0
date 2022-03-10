8be5b79f803400adb29c41fba4202cf9a472caee
alcb1310 <97707578+alcb1310@users.noreply.github.com>
2022-03-10 12:29:21 -0500

    seguridad

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
@@ -34,6 +35,14 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         return ($request->getPathInfo() === '/login' && $request->isMethod('POST'));
     }
     
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        
+        dd('failure');
+        dd ($this->urlGenerator->generate('app_homepage'));
+        // For example:
+        return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+    }
 
     public function authenticate(Request $request): Passport
     {
